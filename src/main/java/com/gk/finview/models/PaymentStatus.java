3af3f1c0c887384ef66db4800fac04684793d317
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2024-06-24 20:48:48 -0300

    feat: add PaymentStatus model

---
+++ src/main/java/com/gk/finview/models/PaymentStatus.java
@@ -0,0 +1,46 @@
+package main.java.com.gk.finview.models;
+
+public class PaymentStatus {
+    private int id;
+    private String name;
+    private String value;
+
+    public PaymentStatus(int id, String name, String value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentStatus{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
