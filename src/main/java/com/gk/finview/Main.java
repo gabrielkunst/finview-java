6b9519f6b8c5e4cd11d79463bb810ab29426d64d
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2024-06-25 09:04:50 -0300

    feat: add main view

--- src/main/java/com/gk/finview/Main.java
+++ src/main/java/com/gk/finview/Main.java
@@ -1,6 +1,6 @@
 package main.java.com.gk.finview;
 
-public class FinviewApplication {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
